78b25dc6babf2b12151da71854c874dab89dea27
suleyilmaz22 <2303013099@student.beykent.edu.tr>
2025-07-25 18:17:41 +0300

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Şule Yılmaz on 25.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
